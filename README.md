9d92d94bf5b7ffa219a27bf142f1e791ebdd2e05
axross <kou@axross.org>
2015-03-05 01:53:24 +0900

    Remove contaminant mixed in generated file

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [WIP] bemmer.js
 
+[![npm](https://img.shields.io/npm/v/npm.svg)](https://www.npmjs.com/package/bemmer)
+
 ## API
 
 ### new Bemmer()
--- bemmer.js
+++ bemmer.js
@@ -4,13 +4,13 @@ var _prototypeProperties = function (child, staticProps, instanceProps) { if (st
 
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
-var ClassNamer = (function () {
-  function ClassNamer() {
+var Bemmer = (function () {
+  function Bemmer() {
     for (var _len = arguments.length, classNames = Array(_len), _key = 0; _key < _len; _key++) {
       classNames[_key] = arguments[_key];
     }
 
-    _classCallCheck(this, ClassNamer);
+    _classCallCheck(this, Bemmer);
 
     this.__classNames = Array.prototype.concat.apply([], classNames.map(function (className) {
       return className.split(/\s/).filter(function (v) {
@@ -19,12 +19,12 @@ var ClassNamer = (function () {
     }));
   }
 
-  _prototypeProperties(ClassNamer, null, {
+  _prototypeProperties(Bemmer, null, {
     element: {
       value: function element() {
         var elementName = arguments[0] === undefined ? "" : arguments[0];
 
-        elementName = elementName.replace(/[_\-\s]{2,}/, "__");
+        elementName = elementName.replace(/[_\-\s]{2,}/g, "__");
         elementName = elementName.replace(/^[_\-\s]{2}/, "");
 
         return this.__join("__" + elementName);
@@ -38,7 +38,7 @@ var ClassNamer = (function () {
 
         if (bool !== void 0 && !bool) {
           return this;
-        }stateName = stateName.replace(/[_\-\s]{2,}/, "--");
+        }stateName = stateName.replace(/[_\-\s]{2,}/g, "--");
         stateName = stateName.replace(/^[_\-\s]{2}/, "");
 
         return this.__join("--" + stateName);
@@ -89,18 +89,14 @@ var ClassNamer = (function () {
           return v + classNamePiece;
         }).join(" ");
 
-        return new ClassNamer(newClassName);
+        return new Bemmer(newClassName);
       },
       writable: true,
       configurable: true
     }
   });
 
-  return ClassNamer;
+  return Bemmer;
 })();
 
-module.exports = ClassNamer;
-
-var className = new ClassNamer("myComponent babel", "unko", "tinpo");
-
-console.log(className.element("-_title_ inpo").state("--isEven", true).state("--isOdd", false).get());
+module.exports = Bemmer;
